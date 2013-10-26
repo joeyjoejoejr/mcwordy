4abb60e001f829d93b903a91aab97cd07d9f4a5c
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-10-25 20:20:24 -0400

    only moosh entity

--- server/src/mcwordy/models/entities.clj
+++ server/src/mcwordy/models/entities.clj
@@ -5,25 +5,4 @@
 
 (declare user)
 
-(defentity moosh
-  (belongs-to user)
-  (has-many comment)
-  (has-many favorite)
-  (prepare hidden-text->boolean))
-
-(defentity user
-  (has-many moosh)
-
-  ;; todo move user param transform function here
-  (prepare
-   (fn [attributes]
-     (-> (transform-when-key-exists
-          attributes
-          {:password #(creds/hash-bcrypt %)})
-         (assoc :roles (str [:user])))))
-  
-  (transform #(deserialize % :roles)))
-
-(defentity user_session
-  (prepare #(merge % {:data (str (:data %))}))
-  (transform #(deserialize % :data)))
+(defentity moosh)
